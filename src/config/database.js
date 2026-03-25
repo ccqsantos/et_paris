f307004c7ded98d389817256ecc97c30fb40f295
ccqsantos <caua0.cicarone@gmail.com>
2026-03-25 13:54:25 -0300

    removed explicit db url

--- src/config/database.js
+++ src/config/database.js
@@ -1,16 +1,30 @@
 import { Sequelize } from "sequelize";
-import path from "node:path";
-import fs from 'node:fs';
+import dotenv from 'dotenv'
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-const sequelize = new Sequelize('postgresql://postgres:antimatter@localhost:5432/et_paris?schema=public');
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const envPath = path.join(__dirname, '../../.env');
+
+dotenv.config({ path: envPath });
+
+const dburl = process.env.DATABASE_URL;
+
+if (!dburl) {
+    console.error('DATABASE_URL environment variable is not defined');
+    console.error('Looking for .env path at ', envPath);
+    process.exit(1);
+}
+
+const sequelize = new Sequelize(dburl);
 
 try{
     await sequelize.authenticate();
     console.log('Authentication succesful');
 }catch(error){
     console.log('Not possible to connect: ', error);
-}finally{
-    sequelize.close();
 }
 
 export default sequelize;
